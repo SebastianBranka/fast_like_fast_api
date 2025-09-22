3a04de021b1a59ac6fc5000f6e08cf243864c55a
SebastianBranka <sebastian.branka.unity@gmail.com>
2025-09-22 22:27:17 +0200

    Review fast api

--- chapter_01/hello.py
+++ chapter_01/hello.py
@@ -1,1 +1,47 @@
-print("hello")
+from fastapi import FastAPI, Body, Header, Response
+import datetime, json, pytest 
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+
+
+@app.get("/hi")
+def greet(who):
+    return f"Welcome? {who}?"
+
+
+@app.post("/")
+def greet(who: str = Header()):
+    return f"Welcome? {who}?"
+
+
+@app.post("/agent")
+def get_agent(user_agent:str = Header()):
+    return f"Your user agent is {user_agent}"
+
+@app.get("/happy")
+def happy(status_code=200):
+    return ":)"
+
+@app.get("/header/{name}/{value}")
+def header(name: str, value: str, response: Response):
+    response.headers[name] = value
+    return "normal body"
+
+@pytest.fixture
+def data():
+    return datetime.datetime.now()
+
+def test_json_dumps(data):
+    with pytest.raises(Exception):
+        _ = json.dumps(data)
+
+def test_encoder(data):
+    out = jsonable_encoder(data)
+    assert out
+    json_out = json.dumps(out)
+    assert json_out
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("hello:app", reload=True)
